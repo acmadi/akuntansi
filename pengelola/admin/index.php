f2980a1c372530cc3cfbc406b7f3722907cdffd4
Serigala Malam <mars.rizta@gmail.com>
2016-06-27 17:07:04 +0700

    tambah halaman admin

---
+++ pengelola/admin/index.php
@@ -0,0 +1,26 @@
+<?php
+include '../../tampilan/header_footer/index.php';
+
+Headers();
+?>
+	<title>Halaman Admin</title>
+
+	<div class="content-wrapper">
+		<!-- Konten -->
+		<section class="content">
+			<div class="box box-default">
+				<div class="box-body">
+		      		<div class="row">
+    					<div class="col-md-12">
+							<fieldset>
+								<legend>Selamat Datang</legend>
+							</fieldset>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+<?php
+Footer();
+?>
